7cf2ee74f618a10c30ad623458b36e6e42ceadf9
Michael-Jorge Gómez Campos <michael.gomez@rosclar.com>
2022-07-23 21:26:12 +0200

    Code cleanup samples

--- dotNET/pdfclown.samples.cli/src/org/pdfclown/samples/cli/PrintingSample.cs
+++ dotNET/pdfclown.samples.cli/src/org/pdfclown/samples/cli/PrintingSample.cs
@@ -1,35 +1,34 @@
-using org.pdfclown.files;
-using org.pdfclown.tools;
 
 using System;
-using System.Collections.Generic;
-using System.Drawing;
+using org.pdfclown.files;
+
+using org.pdfclown.tools;
 
 namespace org.pdfclown.samples.cli
 {
-  /**
-    <summary>This sample demonstrates how to print a PDF document.<summary>
-    <remarks>Note: printing is currently in pre-alpha stage; therefore this sample is
-    nothing but an initial stub (no assumption to work!).</remarks>
-  */
-  public class PrintingSample
-    : Sample
-  {
-    public override void Run(
-      )
+    /**
+      <summary>This sample demonstrates how to print a PDF document.<summary>
+      <remarks>Note: printing is currently in pre-alpha stage; therefore this sample is
+      nothing but an initial stub (no assumption to work!).</remarks>
+    */
+    public class PrintingSample
+      : Sample
     {
-      // 1. Opening the PDF file...
-      string filePath = PromptFileChoice("Please select a PDF file");
-      using(var file = new File(filePath))
-      {
-        // 2. Printing the document...
-        Renderer renderer = new Renderer();
-        bool silent = false;
-        if(renderer.Print(file.Document, silent))
-        {Console.WriteLine("Print fulfilled.");}
-        else
-        {Console.WriteLine("Print discarded.");}
-      }
+        public override void Run(
+          )
+        {
+            // 1. Opening the PDF file...
+            string filePath = PromptFileChoice("Please select a PDF file");
+            using (var file = new File(filePath))
+            {
+                // 2. Printing the document...
+                Renderer renderer = new Renderer();
+                bool silent = false;
+                if (renderer.Print(file.Document, silent))
+                { Console.WriteLine("Print fulfilled."); }
+                else
+                { Console.WriteLine("Print discarded."); }
+            }
+        }
     }
-  }
 }
--- dotNET/pdfclown.samples.cli/src/org/pdfclown/samples/cli/VideoEmbeddingSample.cs
+++ dotNET/pdfclown.samples.cli/src/org/pdfclown/samples/cli/VideoEmbeddingSample.cs
@@ -1,46 +1,42 @@
-using org.pdfclown.documents;
-using org.pdfclown.documents.contents;
-using org.pdfclown.documents.contents.composition;
-using org.pdfclown.documents.contents.objects;
-using org.pdfclown.documents.interaction.annotations;
-using org.pdfclown.files;
-using org.pdfclown.objects;
 
 using System;
-using System.Collections.Generic;
 using System.Drawing;
+using org.pdfclown.documents;
+
+using org.pdfclown.documents.interaction.annotations;
+using org.pdfclown.files;
 
 namespace org.pdfclown.samples.cli
 {
-  /**
-    <summary>This sample demonstrates how to insert screen annotations to display media clips inside
-    a PDF document.</summary>
-  */
-  public class VideoEmbeddingSample
-    : Sample
-  {
-    public override void Run(
-      )
+    /**
+      <summary>This sample demonstrates how to insert screen annotations to display media clips inside
+      a PDF document.</summary>
+    */
+    public class VideoEmbeddingSample
+      : Sample
     {
-      // 1. Instantiate the PDF file!
-      File file = new File();
-      Document document = file.Document;
+        public override void Run(
+          )
+        {
+            // 1. Instantiate the PDF file!
+            File file = new File();
+            Document document = file.Document;
 
-      // 2. Insert a new page!
-      Page page = new Page(document);
-      document.Pages.Add(page);
+            // 2. Insert a new page!
+            Page page = new Page(document);
+            document.Pages.Add(page);
 
-      // 3. Insert a video into the page!
-      new Screen(
-        page,
-        new RectangleF(10, 10, 320, 180),
-        "PJ Harvey - Dress (part)",
-        GetResourcePath("video" + System.IO.Path.DirectorySeparatorChar + "pj_clip.mp4"),
-        "video/mp4"
-        );
+            // 3. Insert a video into the page!
+            new Screen(
+              page,
+              new RectangleF(10, 10, 320, 180),
+              "PJ Harvey - Dress (part)",
+              GetResourcePath("video" + System.IO.Path.DirectorySeparatorChar + "pj_clip.mp4"),
+              "video/mp4"
+              );
 
-      // 4. Serialize the PDF file!
-      Serialize(file, "Video embedding", "inserting screen annotations to display media clips inside a PDF document", "video embedding");
+            // 4. Serialize the PDF file!
+            Serialize(file, "Video embedding", "inserting screen annotations to display media clips inside a PDF document", "video embedding");
+        }
     }
-  }
 }
